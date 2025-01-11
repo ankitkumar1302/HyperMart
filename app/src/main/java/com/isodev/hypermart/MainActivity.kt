e538fe8223b6a54eb3ef0a9c2269d8d162b27a2b
ismailfatihbirak <birak200@gmail.com>
2025-01-11 16:03:29 +0300

    Initial commit

---
+++ app/src/main/java/com/isodev/hypermart/MainActivity.kt
@@ -0,0 +1,179 @@
+package com.isodev.hypermart
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.contentColorFor
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import com.isodev.hypermart.ui.theme.HyperMartTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            HyperMartTheme {
+                Scaffold(modifier = Modifier.fillMaxSize(), bottomBar = {
+                    CustomBottomBar()
+                }) { innerPadding ->
+                    Home(
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Home(modifier: Modifier = Modifier) {
+
+}
+
+@Composable
+fun TopBar(modifier: Modifier = Modifier) {
+
+}
+
+@Composable
+fun CustomBottomBar(
+    modifier: Modifier = Modifier,
+    containerColor: Color = Color.White,
+    contentColor: Color = contentColorFor(containerColor),
+    tonalElevation: Dp = 8.dp,
+    contentPadding: PaddingValues = PaddingValues(),
+    windowInsets: WindowInsets = WindowInsets(0.dp),
+) {
+    Box(
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        BottomAppBar(
+            modifier = modifier,
+            containerColor = containerColor,
+            contentColor = contentColor,
+            tonalElevation = tonalElevation,
+            contentPadding = contentPadding,
+            windowInsets = windowInsets,
+            content = {
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.SpaceEvenly
+                ) {
+                    IconButton(onClick = {}) {
+                        Image(
+                            painter = painterResource(R.drawable.ic_home),
+                            contentDescription = "Home"
+                        )
+                    }
+                    IconButton(onClick = {}) {
+                        Image(
+                            painter = painterResource(R.drawable.dashboard_1),
+                            contentDescription = "Dashboard"
+                        )
+                    }
+                    Spacer(modifier = Modifier.width(73.dp)) 
+                    IconButton(onClick = {}) {
+                        Image(
+                            painter = painterResource(R.drawable.ic_wishlist),
+                            contentDescription = "Wishlist"
+                        )
+                    }
+                    IconButton(onClick = {}) {
+                        Icon(
+                            imageVector = Icons.Default.AccountCircle,
+                            contentDescription = "Account",
+                            tint = colorResource(id = R.color.gray)
+                        )
+                    }
+                }
+            }
+        )
+        Box(
+            modifier = Modifier
+                .align(Alignment.TopCenter)
+                .offset(y = -15.dp)
+                .size(73.dp)
+                .shadow(5.dp, shape = CircleShape)
+                .clip(CircleShape)
+                .background(color = colorResource(id = R.color.white))
+        ) {
+            Box(
+                modifier = Modifier
+                    .align(Alignment.Center)
+                    .size(63.dp)
+                    .clip(CircleShape)
+                    .background(color = colorResource(id = R.color.orange))
+            ) {
+                Image(
+                    painter = painterResource(R.drawable.vector),
+                    contentDescription = "Center Icon",
+                    modifier = Modifier.align(Alignment.Center)
+                )
+            }
+        }
+    }
+}
+
+
+
+@Composable
+fun CustomPager(modifier: Modifier = Modifier) {
+
+}
+
+@Composable
+fun SearchBarAndLocation(modifier: Modifier = Modifier) {
+
+}
+
+@Composable
+fun CategoriesLazyRow(modifier: Modifier = Modifier) {
+
+}
+
+@Composable
+fun PreviousOrder(modifier: Modifier = Modifier) {
+
+}
+
+@Composable
+fun PopularDealsLazyGrid(modifier: Modifier = Modifier) {
+
+}
