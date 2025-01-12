bd8a9fe7518c5e04198a7ff138689feac4dad6bb
ismailfatihbirak <birak200@gmail.com>
2025-01-12 12:43:43 +0300

    TopBar and SearchBarAndLocation

--- app/src/main/java/com/isodev/hypermart/MainActivity.kt
+++ app/src/main/java/com/isodev/hypermart/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -20,14 +21,25 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.KeyboardArrowRight
 import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.contentColorFor
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -44,9 +56,13 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             HyperMartTheme {
-                Scaffold(modifier = Modifier.fillMaxSize(), bottomBar = {
-                    CustomBottomBar()
-                }) { innerPadding ->
+                Scaffold(modifier = Modifier.fillMaxSize(),
+                    bottomBar = {
+                        CustomBottomBar()
+                    },
+                    topBar = {
+                        TopBar()
+                    }) { innerPadding ->
                     Home(
                         modifier = Modifier.padding(innerPadding)
                     )
@@ -58,12 +74,33 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Home(modifier: Modifier = Modifier) {
-
+    SearchBarAndLocation(modifier = modifier)
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TopBar(modifier: Modifier = Modifier) {
-
+    TopAppBar(
+        title = {
+            Row(
+                modifier = modifier
+                    .fillMaxWidth()
+                    .padding(start = 8.dp, end = 8.dp),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Image(painter = painterResource(R.drawable.hypermart), contentDescription = "")
+                Row(
+                    modifier = Modifier.padding(end = 12.dp),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Image(painter = painterResource(R.drawable.group_6), contentDescription = "")
+                    Spacer(modifier = Modifier.width(4.dp))
+                    Image(painter = painterResource(R.drawable.group_5), contentDescription = "")
+                }
+            }
+        }
+    )
 }
 
 @Composable
@@ -157,18 +194,92 @@ fun CustomBottomBar(
 }
 
 
-
-
-
 @Composable
 fun CustomPager(modifier: Modifier = Modifier) {
 
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SearchBarAndLocation(modifier: Modifier = Modifier) {
-
+    var searchText by remember { mutableStateOf("") }
+    Column(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(16.dp)
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Image(
+                painter = painterResource(R.drawable.group_7),
+                contentDescription = null,
+                modifier = Modifier
+                    .padding(end = 8.dp)
+            )
+            Column(
+                modifier = Modifier.weight(1f)
+            ) {
+                Text(
+                    text = "Bengaluru",
+                    color = Color.Black
+                )
+                Text(
+                    text = "BTM Layout, 500628",
+                    color = Color.Gray
+                )
+            }
+            Icon(
+                imageVector = Icons.Default.KeyboardArrowRight,
+                contentDescription = null,
+                tint = Color.Gray,
+                modifier = Modifier.size(24.dp)
+            )
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+        TextField(
+            value = searchText,
+            onValueChange = { searchText = it },
+            placeholder = {
+                Text(
+                    text = "Search Anything...",
+                    color = Color.Gray
+                )
+            },
+            leadingIcon = {
+                Image(
+                    painter = painterResource(R.drawable.search__6__1),
+                    contentDescription = null,
+                )
+            },
+            trailingIcon = {
+                Row (verticalAlignment = Alignment.CenterVertically){
+                    Image(painter = painterResource(R.drawable.vector_3), contentDescription = "")
+                    Image(
+                        painter = painterResource(R.drawable.mic__1__1),
+                        contentDescription = null,
+                        modifier = Modifier.size(24.dp).padding(start = 4.dp)
+                    )
+                }
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(
+                    color = colorResource(R.color.search_bar_background),
+                    shape = RoundedCornerShape(12.dp)
+                )
+                .padding(horizontal = 8.dp, vertical = 2.dp),
+            colors = TextFieldDefaults.textFieldColors(
+                containerColor = colorResource(R.color.search_bar_background),
+                focusedIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent
+            )
+        )
+    }
 }
+
+
 
 @Composable
 fun CategoriesLazyRow(modifier: Modifier = Modifier) {
